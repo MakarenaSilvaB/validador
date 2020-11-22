95b9d627b60ab5497bd6280054d140383f8ab4df
Makarena Silva <makarena.silva@alumnos.uv.cl>
2020-11-22 17:56:29 -0300

    Actualizando

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -7,7 +7,8 @@ class MiComponente extends Component  {
     return (
      <div>
         <p>hello world 2 mtf {this.props.mensaje}</p>
-        <input type="button" value="presione aqui"> </input>
+        <input type="button" value="CLICK AQUÍ" />
+        <img src="https://lh3.googleusercontent.com/proxy/NhaA_5avs1g0u5VIkB5UrDKlxn8A1U5vpAFcP3Ho8T1VK2w0gIl4QuA1gG01FXMIGerjhXYTYjJGOuQL1PKcAlqNjb8Kfq7gv3mkktnnXWdOwGc" />
 
      </div>
     );
