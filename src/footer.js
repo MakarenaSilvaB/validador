5e8cdc573a4a3f8b5224e41e1dcd7d7e5df9b623
Makarena Silva <makarena.silva@alumnos.uv.cl>
2020-11-23 17:06:00 -0300

    Actualizados enlaces

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <a href="https://hub.docker.com/r/makarenasilva/validat-e" className="App-link">Docker Hub |</a>
+    <a href="https://hub.docker.com/r/makarenasilva/mi-validador-dw" className="App-link">Docker Hub |</a>
     <a href="https://dev.azure.com/ArquitecturaSW/Docker%20y%20React" className="App-link">| Azure DevOps</a>
     <p className="App-link">~By: Makarena Silva B.</p>
   </div>
